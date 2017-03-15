5040696be3732935ed00459e0f1bb92de031c7ac
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2017-03-15 23:34:37 +0000

    setup riak run script

--- riak/riak_setup.sh
+++ riak/riak_setup.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
 
 # Set ownership
-chown riak:riak /var/lib/riak /var/log/riak
-chmod 755 /var/lib/riak /var/log/riak
+#chown riak:riak /var/lib/riak /var/log/riak
+#chmod 755 /var/lib/riak /var/log/riak
 
 # Open file descriptor limit
 ulimit -n 65536
 
 # Start Riak
-exec "/usr/sbin/riak start"
+service riak start
+
+# Tail to keep it in the foreground
+tail -f /var/log/riak/console.log
+
